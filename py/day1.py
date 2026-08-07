7f3b5dc7a177b65420762fc2dcdc1245f275726d
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-07 11:47:41 -0300

    Mais alguns exercicios

--- py/day1.py
+++ py/day1.py
@@ -1,19 +1,56 @@
-# - Aprendendo a usar o input e f-strings
+# - Aprendendo a usar o input, f-strings e fixação do :.2f
 
 # Exercicio 1 - 10xp
+print("Exercicio 1 concluido")
 #nome = input("Qual seu nome? ")
 #idade = int(input(f"Olá {nome}, quantos anos você tem? "))
 #print("")
 #print("Informação guardada com sucesso!")
 #print(f"O usuário {nome} tem {idade} anos...")
-print("Exercicio 1 concluido")
-
-# Exercicio 2 - 10xp
-num1 = int(input("Digite o primeiro valor: "))
-num2 = int(input("Digite o segundo valor: "))
 print("")
 
-print(f"Adição: {num1} + {num2} = {num1+num2}")
-print(f"Subtração: {num1} - {num2} = {num1-num2}")
-print(f"Multiplicação: {num1} x {num2} = {num1*num2}")
-print(f"Divisão: {num1}/{num2} = {num1/num2}")
+# Exercicio 2 - 10xp
+print("Exercicio 2 concluido")
+#num1 = int(input("Digite o primeiro valor: "))
+#num2 = int(input("Digite o segundo valor: "))
+#print("")
+
+#print(f"Adição: {num1} + {num2} = {num1+num2}")
+#print(f"Subtração: {num1} - {num2} = {num1-num2}")
+#print(f"Multiplicação: {num1} x {num2} = {num1*num2}")
+#print(f"Divisão: {num1}/{num2} = {num1/num2}")
+print("")
+
+# Exercicio 3 - 10xp
+print("Exercicio 3 concluido")
+#print("Veja quanto gastou...")
+#prod = input("Oque você comprou? ")
+#qnt = int(input(f"Ótimo! e qual a quantidade de {prod +"s"}? "))
+#preco = float(input(f"Por ultimo, qual preço do item? "))
+#print("")
+#print(f"Você gatou exatamente {preco*qnt} na compra de {qnt} {prod +"s"}!")
+print("")
+
+# Exercicio 4 - 20xp
+print("Exercicio 4 concluido")
+#print("Veja seu boletim e média bimestral!")
+#print("")
+#nome = input("Qual seu nome? ")
+#nota_pyt = float(input("Qual sua nota em Python? "))
+#nota_java = float(input("Qual sua nota em Java? "))
+#nota_css = float(input("Qual sua nota em CSS? "))
+#media = (nota_pyt + nota_java + nota_css)/3
+#print("")
+#print("===============")
+#print("    BOLETIM    ")
+#print("===============")
+#print(f"Aluno: {nome}")
+#print("---------------")
+#print(f"Python --- {nota_pyt:.2f}")
+#print(f"Java ----- {nota_java:.2f}")
+#print(f"CSS ------ {nota_css:.2f}")
+#print("---------------")
+#print(f"Média ---- {media:.2f}")
+print("")
+
+#
