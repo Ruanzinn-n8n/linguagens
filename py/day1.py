1396183f616b60bfe38173c10f68cd3d232d7b2d
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-07 11:06:53 -0300

    Create day1.py

---
+++ py/day1.py
@@ -0,0 +1,19 @@
+# - Aprendendo a usar o input e f-strings
+
+# Exercicio 1 - 10xp
+#nome = input("Qual seu nome? ")
+#idade = int(input(f"Olá {nome}, quantos anos você tem? "))
+#print("")
+#print("Informação guardada com sucesso!")
+#print(f"O usuário {nome} tem {idade} anos...")
+print("Exercicio 1 concluido")
+
+# Exercicio 2 - 10xp
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("Digite o segundo valor: "))
+print("")
+
+print(f"Adição: {num1} + {num2} = {num1+num2}")
+print(f"Subtração: {num1} - {num2} = {num1-num2}")
+print(f"Multiplicação: {num1} x {num2} = {num1*num2}")
+print(f"Divisão: {num1}/{num2} = {num1/num2}")
