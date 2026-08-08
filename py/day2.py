074b5b213f5c7223e7a9aa858f8a135a76d6388d
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-08 12:48:27 -0300

    Create day2.py

---
+++ py/day2.py
@@ -0,0 +1,12 @@
+# aprendendo mais sobre if, elif e else. suas condições e operadores.
+
+# Exercicio 1 - 10xp
+print("Exercicio 1 concluido")
+print("")
+idade = int(input("Quantos anos você tem? "))
+
+if idade >= 18:
+    print("Você é maior de idade!")
+else:
+    print("Você é menor de idade!")
+print("")
