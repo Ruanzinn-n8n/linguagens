6a78c3cb8a4660569eaac18d3d896c043240f24c
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-10 07:58:31 -0300

    alguns exercicios

--- py/day3.py
+++ py/day3.py
@@ -1,2 +1,48 @@
-input("Qual seu nome?")
-print("Você Samira Stefani, é o amor da minha vida, hoje e sempre!!! A mais mais, mais diva, mais linda sempre.")
+# Aprendendo sobre estruturas de repetição, como "while" e "for" e a lógica através dela.
+
+# Exercicio 1 - 10xp
+print("Exercicio 1 concluido")
+"""print("")
+for i in range(1, 11):
+    print(i)
+"""
+print("")
+
+# Exercicio 2 - 10xp
+print("Exercicio 2 concluido")
+"""print("")
+print("-----------------------------")
+n = int(input("Digite um numero inteiro: "))
+n += 1
+print("-----------------------------")
+
+for i in range(1, n):
+    print(i)
+"""
+print("")
+
+# Exercicio 3 - 15xp
+print("Exercicio 3 concluido")
+"""print("")
+tab = int(input("Qual tabuada deseja saber? "))
+print("------------------------------------")
+
+for i in range(1, 11):
+    resul = tab
+    resul *= i
+    print(f"{tab}x{i} = {resul}")
+"""
+print("")
+
+# Exercicio 4 - 20xp
+print("Exercicio 4")
+print("")
+num = int(input("Digite um número inteiro: "))
+print("------------------------------------")
+num += 1
+soma = 0
+
+for i in range(1, num):
+    soma += i
+print(f" O resultado é {soma}.")
+print("")
