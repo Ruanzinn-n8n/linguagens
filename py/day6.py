6658baada8cb125be44720597f12c5199f396ddf
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-27 20:21:25 -0300

    Update day6.py

--- py/day6.py
+++ py/day6.py
@@ -1,18 +1,17 @@
 print("Exercicio 1 - concluido")
-"""print("")
+print("")
 a, b = map(int, input("Digite 2 valores: ").split())
 print(a + b)
-"""
+
 
 print("Exercicio 2 - concluido")
-"""print("")
+print("")
 a, b, c = map(int, input("Digite 3 valores: ").split())
 media = (a+b+c)/3
 print(f"{media:.2f}")
-"""
 
 print("Exercicio 3 - concluido")
-"""print("")
+print("")
 def maior_menor(lista):
     maior = lista[0]
     menor = lista[0]
@@ -28,10 +27,9 @@ n = int(input("Digite uma quantidade N: "))
 numeros = list(map(int, input("Digite N numeros: ").split()))
 
 maior_menor(numeros)
-"""
 
 print("Exercicio 4 - concluido")
-"""print("")
+print("")
 
 def inverter(lista):
     lista_inv = []
@@ -43,10 +41,9 @@ def inverter(lista):
 
 numeros = list(map(int, input("Digite os numeros: ").split()))
 inverter(numeros)
-"""
 
 print("Exercicio 5 - concluido")
-"""print("")
+print("")
 
 def eh_par(lista):
     cont = 0
@@ -60,10 +57,9 @@ print(f"Digite {n} números:")
 numeros = list(map(int, input().split()))
 
 eh_par(numeros)
-"""
 
 print("Exercicio 6 - concluido")
-"""print("")
+print("")
 
 def pos_neg(lista):
     p = 0
@@ -88,13 +84,12 @@ print(f"Digite {n} números:")
 numeros = list(map(int, input().split()))
 
 pos_neg(numeros)
-"""
 
 print("Exercicio 7 -")
 print("")
 # Eu não fiz nenhum exercicio anterior, quando disse pra refazer as questões com base no que meu amigo falou você considerou que eu havia feito as questões, mas como era pra refazer então n fiz. após corrigir esse me passe outros exercicios ensinando oque vc tinha dito antes e ai depois a gnt avança.
 
-print("Exercicio 8 -")
+print("Exercicio 8 - concluido")
 print("")
 
 def ingres(n):
@@ -113,4 +108,13 @@ while dc == 0:
         print("Número inválido! Digite outro:")
     else:
         dc = 1
-ingres(num)    
+ingres(num)
+
+print("Exercicio 9 -")
+print("")
+
+# N faço a menor ideia pq n sei mt sobre numeros binários kkkk me ensine junto com as outras coisas e já coloque nos próximos exercicios.
+
+print("Exercicio 10 -")
+print("")
+# Infelismente também não sei kkk mas acredito que vai usar aquele metodo ::: que você comentou antes, porém como n li os ultimos exercicios também n sei, me ensina sobre e também põe no próximo
