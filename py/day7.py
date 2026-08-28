03febf651028e2e1a967769a6e34d91df4362725
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-08-28 19:49:48 -0300

    estudoooo

--- py/day7.py
+++ py/day7.py
@@ -1,7 +1,7 @@
 #vou usar os comentários apenas para voce indentificar qual exercicio é qual, pode ignorar eles.
 
 # Exercicio 1
-"""n = int(input())
+n = int(input())
 numeros = []
 for i in range(n):
     numeros.append(int(input()))
@@ -9,27 +9,61 @@ numeros.sort()
 print(f"Maior: {numeros[n-1]}")
 print(f"Menor: {numeros[0]}")
 print(numeros)
-"""
 
 # Exercicio 2
-"""n = int(input())
+n = int(input())
 nums = list(map(int, input().split()))
 dif = set(nums)
 print(len(dif))
-"""
 
 # Exercicio 3
-"""numeros = list(map(int, input().split()))
+numeros = list(map(int, input().split()))
 maior = numeros[0]
+i_maior = 0
 for i, numero in enumerate(numeros):
     if numero > maior:
         maior = numero
+        i_maior = i
 print(i, maior)
 #travei em mostrar a primeira vez
-"""
 
 # Exercicio 4
 nomes = list(input().split())
 notas = list(map(int, input().split()))
 for nome, nota in zip(nomes, notas):
     print(nome, nota)
+
+# Exercicio 5
+def freq(n):
+    uni = {}
+    for i in n:
+        if i in uni:
+            uni[i] += 1
+        else:
+            uni[i] = 1
+    return uni
+
+nums = list(map(int, input().split()))
+resultado = freq(nums)
+print(resultado)
+
+# Exercicio 6
+n = list(map(int, input().split()))
+unicos = set(n)
+rev = []
+for i in unicos:
+    rev.append(i)
+rev.sort(reverse=True)
+print(rev[1])
+
+# Exercicio 7
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+for i in list2:
+    list1.append(i)
+limpa = set(list1)
+list3 = []
+for i in limpa:
+    list3.append(i)
+list3.sort()
+print(list3)
--- py/teste.py
+++ py/teste.py
@@ -1,32 +1,13 @@
-print("Rec 1 - concluida")
-print("")
+def freq(n):
+    uni = {}
+    for i in n:
+        if i in uni:
+            uni[i] += 1
+        else:
+            uni[i] = 1
+    return uni
 
-def num_bin(num):
-    bin = []
-    while num != 0:
-        sobra = num % 2
-        bin.append(sobra)
-        num = num // 2
-    return bin
-def inverter(lista):
-    bin = []
-    tam = len(lista) -1
-    for i in range(len(lista)):
-        n_oposto = tam -i
-        bin.append(lista[n_oposto])
-    return bin
-
-n = 0
-while n <= 0:
-    n = int(input("Digite o número para conversão: "))
-    if n <= 0:
-        print("Número inválido, digite um número maior que 0!")
-
-num = num_bin(n)
-n_binario = inverter(num)
-str_bin = []
-for i in n_binario:
-    n_bin = str(i)
-    str_bin.append(n_bin)
-j_bin = "".join(str_bin)
-print(j_bin)
+nums = list(map(int, input().split()))
+resultado = freq(nums)
+for i in resultado:
+    print(f"{i}: {resultado[i]}")
