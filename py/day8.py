5515eb7c3c286e72a3956fb71e4cd7dde48c1259
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-09-04 10:49:20 -0300

    Update day8.py

--- py/day8.py
+++ py/day8.py
@@ -1,1 +1,12 @@
 # Aprendendo sobre matrizes
+
+print("Exercicio 1")
+matriz = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90],
+]
+print(matriz[1][1])
+print(matriz[1])
+for i in matriz:
+    print(i)
